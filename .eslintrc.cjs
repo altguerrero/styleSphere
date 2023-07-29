9a92823abec2c7f40a96314324f6f6bb6d32631f
altguerrero <josealberto.guerreroayala@gmail.com>
2023-07-29 01:06:48 -0500

    :tada: Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    'react-refresh',
+    '@typescript-eslint',
+    'react-hooks',
+    'prettier',
+    'simple-import-sort',
+  ],
+  rules: {
+    'no-duplicate-imports': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'error',
+    'no-alert': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/no-unknown-property': 'off',
+    'react/no-unescaped-entities': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
